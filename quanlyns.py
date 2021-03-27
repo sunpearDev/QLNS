afd9bee4ccbd0566cf24fb17467ea2401a669515
SunPear <sunpear.sp@gmail.com>
2021-03-27 21:16:32 +0700

    make integrity binding

--- quanlyns.py
+++ quanlyns.py
@@ -7,18 +7,22 @@ class phongban (osv.osv):
         # cac thuoc tinh cua lop danh muc hoc phan
         'name': fields.char('Tên phòng ban', size=25, required=True, translate=True),
         'ids': fields.char('Mã phòng ban', size=25, required=True, translate=True),
+      
+        
     }
     
 
 
-class nhanvien(osv.osv):
-    _name = 'quanlyns.nhanvien'
+class quanlyns(osv.osv):
+    _name = 'quanlyns.quanlyns'
     _columns = {
-        'manhanvien': fields.char('Mã nhân viên', size=25, required=True, translate=True),
         'tennhanvien': fields.char('Tên nhân viên', size=25, required=True, translate=True),
+        'manhanvien': fields.char('Mã nhân viên', size=25, required=True, translate=True),
         'sdt': fields.char('SĐT', size=25, required=True, translate=True),
-        'phongban': fields.many2one('quanlyns.phongban', "Tên phòng ban", help="Phòng ban"),
+        'phongban': fields.many2one('quanlyns.phongban', "Tên phòng ban", help="Phòng ban", required=True),
+        'gender': fields.selection([('nam', 'Nam'),('nu', 'Nữ')], 'Giới tính'),
+        'address': fields.char('Địa chỉ', size=200, required=True, translate=True),
         
     }
 phongban()
-nhanvien() #tao 1 the hien cho lop quanlyns_quanlyns
+quanlyns() #tao 1 the hien cho lop quanlyns_quanlyns
